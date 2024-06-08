ed36d090091b5c438b45fa6eff3123f54f9d1c06
juancodev <montillasanchezjuancarlos@gmail.com>
2024-06-08 16:59:56 -0400

    🚧 New properties in the component of Image
    New properties in the component of Image from Next/image - prority: is the lazyloading of the images - quality: is the quality of the image, default is  75%

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,10 +1,18 @@
 import styles from "./Description.module.sass";
+import Image from "next/image";
 
 export const Description = () => {
   return (
     <>
       <section className={styles.Description}>
-        <img src="/images/bookshop.jpg" alt="Book Shop Image" />
+        <Image
+          src="/images/bookshop.jpg"
+          alt="Book Shop Image"
+          width={500}
+          height={300}
+          // priority={false}
+          //quality={100}
+        />
         <div className={styles.Description__text}>
           <h2>The best Book Shop in the world</h2>
           <p>Your knowledge is power and get more reading!</p>
