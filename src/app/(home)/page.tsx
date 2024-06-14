e0ab03681b3bc8231f7b4353047f563b902c4b68
juancodev <montillasanchezjuancarlos@gmail.com>
2024-06-14 15:31:43 -0400

    🚧 Seo - metadata, Sanitize-html

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,10 @@
 import { MainProduct } from "app/components/home/MainProduct";
+import { Metadata } from "next";
+
+export const metada: Metadata = {
+  title: "Book Shop",
+  description: "Book Shop online for the mind",
+};
 
 export default function Home() {
   return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,25 @@
 import { ProductView } from "app/components/product/ProductView/ProductView";
 import { getProducts } from "app/services/shopify/products";
+import { redirect } from "next/navigation";
+
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  // Meta data
+  const id = searchParams.id;
+  const products = await getProducts(id);
+
+  if (products) {
+    const product = products[0];
+
+    return {
+      title: product.title,
+      description: product.description,
+      keywords: product.tags,
+      openGraph: {
+        images: [product.image],
+      },
+    };
+  }
+}
 
 export default async function ProductPage(props: ProductPageProps) {
   const id = props.searchParams.id;
@@ -7,6 +27,10 @@ export default async function ProductPage(props: ProductPageProps) {
 
   if (products) {
     const product = products[0];
+
+    if (!id) {
+      redirect("/store");
+    }
 
     return (
       <>
